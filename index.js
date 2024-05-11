f9be0088112a678acb7d4a7022e2cf08bad3ed16
Absai Mike Ochango <mikeochango3@gmail.com>
2024-05-11 22:26:36 +0000

    Created the API

--- index.js
+++ index.js
@@ -24,6 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  // Get the date parameter from the request URL
+  const dateString = req.params.date;
+  
+  // If no date is provided, return current date
+  if (!dateString) {
+    return res.json({ unix: Date.now(), utc: new Date().toUTCString() });
+  }
+  
+  // If a valid date string is provided
+  const date = new Date(dateString);
+  if (!isNaN(date.getTime())) {
+    return res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+  
+  // If an invalid date string is provided
+  return res.json({ error: "Invalid Date" });
+});
+
+
 
 
 // Listen on port set in environment variable or default to 3000
